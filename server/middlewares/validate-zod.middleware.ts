dac55f03368f5c5279f7d2869df75d45dd6c7f40
Ahmad Mayallo <ahmadmayallo02@gmail.com>
2025-11-15 15:23:31 +0200

    Update validate zod middleware

--- server/middlewares/validate-zod.middleware.ts
+++ server/middlewares/validate-zod.middleware.ts
@@ -1,19 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../utils/appError";
 import { NOT_FOUND } from "../utils/statusCodes";
-import { ZodType } from "zod";
+import { treeifyError, ZodType } from "zod";
 
 export const validateZod =
   (schema: ZodType) => (req: Request, res: Response, next: NextFunction) => {
+    req.body.price = +req.body.price;
+    req.body.stock = +req.body.stock;
+    req.body.discount = +req.body.discount;
     const result = schema.safeParse(req.body);
-
-    if (!result.success)
-      throw new AppError(
-        "Validation failed",
-        NOT_FOUND,
-        result.error.flatten().fieldErrors
-      );
-
+    if (!result.success) {
+      const errors = (treeifyError(result.error) as any).properties;
+      throw new AppError("Validation failed", NOT_FOUND, errors);
+    }
     req.body = result.data;
     next();
   };
